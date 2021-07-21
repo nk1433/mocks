369bb62defaf2219d9431b73f921b96b2754074e
Nitheshkumar <iamnk1433@gmail.com>
2021-07-21 15:53:07 +0530

    Introduced templates in parser.

--- index.js
+++ index.js
@@ -3,28 +3,15 @@ const {rndBetween} = require("@laufire/utils/random");
 
 const parser = fakerParser({})
 const task = () => {
-    return parser({
+    const template = () => ({
         name: "name/firstName",
         cost: () => rndBetween(0,10),
         tasks: [
-            () => rndBetween(0,100),
-            {
-                name: "name/firstName",
-                tasks: [
-                    () => rndBetween(0,2),
-                    {
-                        age: () => rndBetween(1,99),
-                        tasks: [
-                            () => rndBetween(0,2),
-                            {
-                                company: "company/companyName"
-                            }
-                        ]
-                    }
-                ]
-            }
+            () => rndBetween(0,2),
+            template
         ]
-    })
+    });
+    return parser(template);
 };
 
 module.exports = {
